9cc4d3e408ec63439362d1de2a313299963e0395
antoinewdg <antoinewendlinger@gmail.com>
2017-01-24 12:16:50 +0100

    Improve loops

--- include/cv_utils/loops.h
+++ include/cv_utils/loops.h
@@ -7,10 +7,23 @@
 
 namespace cvu {
     template<class T, class Func>
-    void for_each_pixel(const Mat_ <T> &m, Func f) {
+    void for_each_pixel(Mat_ <T> &m, Func f) {
+        T *ptr = m[0];
         for (int i = 0; i < m.rows; i++) {
             for (int j = 0; j < m.cols; j++) {
-                f(i, j);
+                f(i, j, *ptr);
+                ptr++;
+            }
+        }
+    };
+
+    template<class T, class Func>
+    void for_each_pixel(const Mat_ <T> &m, Func f) {
+        const T *ptr = m[0];
+        for (int i = 0; i < m.rows; i++) {
+            for (int j = 0; j < m.cols; j++) {
+                f(i, j, *ptr);
+                ptr++;
             }
         }
     };
--- test/test_loops.cpp
+++ test/test_loops.cpp
@@ -1,15 +1,42 @@
+#include <vector>
+
 #include "test_common.h"
 
 TEST_CASE("for_each_pixel") {
     Mat_<Vec3b> image = load_color(assets_dir() + "/colors.png");
-    int count = 0;
 
-    for_each_pixel(image, [&count, &image](int i, int j) {
-        REQUIRE(count % image.cols == j);
-        REQUIRE(count / image.cols == i);
-        count++;
-    });
 
-    REQUIRE(count == image.cols * image.rows);
+    std::vector<Vec3b> expected = {
+            Vec3b(0, 0, 0), Vec3b(0, 0, 255), Vec3b(255, 255, 255),
+            Vec3b(255, 0, 0), Vec3b(0, 255, 0), Vec3b(255, 255, 255),
+            Vec3b(255, 255, 255), Vec3b(255, 255, 255), Vec3b(255, 255, 255)
+    };
+
+    int width = image.cols;
+    SECTION("regular") {
+        std::vector<Vec3b> actual;
+        for_each_pixel(image, [&actual, width](int i, int j, Vec3b &v) {
+            REQUIRE(actual.size() % width == j);
+            REQUIRE(actual.size() / width == i);
+            actual.push_back(v);
+        });
+
+
+        REQUIRE(actual == expected);
+    }
+
+    SECTION("const") {
+        const Mat_<Vec3b> &ref = image;
+        std::vector<Vec3b> actual;
+        for_each_pixel(ref, [&actual, width](int i, int j, const Vec3b &v) {
+            REQUIRE(actual.size() % width == j);
+            REQUIRE(actual.size() / width == i);
+            actual.push_back(v);
+        });
+
+
+        REQUIRE(actual == expected);
+    }
+
 
 }
