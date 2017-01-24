88dca075cd245fb237d7fb21b56d2eea444f37f7
antoinewdg <antoinewendlinger@gmail.com>
2017-01-24 11:10:46 +0100

    Fix IDE errors

--- include/cv_utils/file_io.h
+++ include/cv_utils/file_io.h
@@ -12,51 +12,64 @@
 #include "common.h"
 
 namespace cvu {
+
+    namespace priv {
+        template<class T>
+        inline Mat_ <T> load_color(string name);
+
+        template<class T>
+        inline Mat_ <T> load_grayscale(string name);
+
+
+        template<>
+        inline Mat_ <Vec3b> load_color(string name) {
+            Mat_<Vec3b> image = cv::imread(name);
+            if (image.empty())                      // Check for invalid input
+            {
+                cerr << "Could not open or find " << name << endl;
+            }
+            return image;
+        }
+
+        template<>
+        inline Mat_ <Vec3f> load_color(string name) {
+            Mat_<Vec3b> original = load_color<Vec3b>(name);
+            Mat_<Vec3f> result;
+            original.convertTo(result, CV_32FC3, 1.f / 255);
+            return result;
+        }
+
+        template<>
+        inline Mat_ <uchar> load_grayscale(string name) {
+            Mat_<uchar> image = cv::imread(name, CV_LOAD_IMAGE_GRAYSCALE);
+            if (image.empty())                      // Check for invalid input
+            {
+                cerr << "Could not open or find " << name << endl;
+            }
+            return image;
+        }
+
+        template<>
+        inline Mat_<float> load_grayscale(string name) {
+            Mat_<uchar> original = load_grayscale<uchar>(name);
+            Mat_<float> result;
+            original.convertTo(result, CV_32F, 1.f / 255);
+            return result;
+        }
+    }
+
     template<class T=Vec3b>
-    inline Mat_ <T> load_color(string name);
+    inline Mat_ <T> load_color(string name) {
+        return priv::load_color<T>(name);
+    }
 
     template<class T=uchar>
-    inline Mat_ <T> load_grayscale(string name);
-
-
-    template<>
-    inline Mat_ <Vec3b> load_color(string name) {
-        Mat_<Vec3b> image = cv::imread(name);
-        if (image.empty())                      // Check for invalid input
-        {
-            cerr << "Could not open or find " << name << endl;
-        }
-        return image;
-    }
-
-    template<>
-    inline Mat_ <Vec3f> load_color(string name) {
-        Mat_<Vec3b> original = load_color(name);
-        Mat_<Vec3f> result;
-        original.convertTo(result, CV_32FC3, 1.f / 255);
-        return result;
-    }
-
-    template<>
-    inline Mat_ <uchar> load_grayscale(string name) {
-        Mat_<uchar> image = cv::imread(name, CV_LOAD_IMAGE_GRAYSCALE);
-        if (image.empty())                      // Check for invalid input
-        {
-            cerr << "Could not open or find " << name << endl;
-        }
-        return image;
-    }
-
-    template<>
-    inline Mat_<float> load_grayscale(string name) {
-        Mat_<uchar> original = load_grayscale(name);
-        Mat_<float> result;
-        original.convertTo(result, CV_32F, 1.f / 255);
-        return result;
+    inline Mat_ <T> load_grayscale(string name) {
+        return priv::load_grayscale<T>(name);
     }
 
     inline Mat_<bool> load_binary(string name) {
-        Mat_<uchar> original = load_grayscale<uchar>(name);
+        Mat_<uchar> original = load_grayscale(name);
         Mat_<bool> out;
         cv::threshold(original, out, 0.0001, 1, CV_8U);
         return (original > 0) / 255;
