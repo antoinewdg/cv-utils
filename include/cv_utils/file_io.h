8ffea1d754fbaac3835d2d186759103ab3aabd42
antoinewdg <antoinewendlinger@gmail.com>
2017-01-24 10:20:29 +0100

    Finalize file io

--- include/cv_utils/file_io.h
+++ include/cv_utils/file_io.h
@@ -7,12 +7,17 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
 
 #include "common.h"
 
 namespace cvu {
     template<class T=Vec3b>
     Mat_<T> load_color(string name);
+
+    template<class T=uchar>
+    Mat_<T> load_grayscale(string name);
+
 
     template<>
     Mat_<Vec3b> load_color(string name) {
@@ -31,6 +36,30 @@ namespace cvu {
         original.convertTo(result, CV_32FC3, 1.f / 255);
         return result;
     }
+
+    template<>
+    Mat_<uchar> load_grayscale(string name) {
+        Mat_<uchar> image = cv::imread(name, CV_LOAD_IMAGE_GRAYSCALE);
+        if (image.empty())                      // Check for invalid input
+        {
+            cerr << "Could not open or find " << name << endl;
+        }
+        return image;
+    }
+
+    template<>
+    Mat_<float> load_grayscale(string name) {
+        Mat_<uchar> original = load_grayscale(name);
+        Mat_<float> result;
+        original.convertTo(result, CV_32F, 1.f / 255);
+        return result;
+    }
+
+    Mat_<bool> load_binary(string name) {
+        Mat_<uchar> original = load_grayscale(name);
+        return Mat_<bool>(original > 0);
+    }
+
 }
 
 #endif //CV_UTILS_FILE_IO_H
--- test/test_file_io.cpp
+++ test/test_file_io.cpp
@@ -23,3 +23,33 @@ TEST_CASE("load_color") {
     }
 
 }
+
+TEST_CASE("load_grayscale") {
+
+    SECTION("uchar") {
+        Mat_<uchar> image = load_grayscale(assets_dir() + "/colors.png");
+        REQUIRE(image(0, 0) == 0);
+        REQUIRE(image(1, 0) == 29);
+        REQUIRE(image(0, 1) == 76);
+        REQUIRE(image(1, 1) == 149);
+        REQUIRE(image(2, 1) == 255);
+    }
+
+    SECTION("float") {
+        Mat_<float> image = load_grayscale<float>(assets_dir() + "/colors.png");
+        REQUIRE(image(0, 0) == 0.f);
+        REQUIRE(image(1, 0) == Approx(0.114f).epsilon(0.01));
+        REQUIRE(image(0, 1) == Approx(0.299f).epsilon(0.01));
+        REQUIRE(image(1, 1) == Approx(0.587f).epsilon(0.01));
+        REQUIRE(image(2, 1) == 1.f);
+    }
+}
+
+TEST_CASE("load_binary") {
+    Mat_<bool> mask = load_binary(assets_dir() + "/colors.png");
+    REQUIRE(!mask(0, 0));
+    REQUIRE(!mask(1, 0));
+    REQUIRE(!mask(0, 1));
+    REQUIRE(!mask(1, 1));
+    REQUIRE(mask(2, 1));
+}
